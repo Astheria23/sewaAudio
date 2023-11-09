7552f130f23d5651c1f1690d00223fc99348ec45
Octa Toriq <119672685+Astheria23@users.noreply.github.com>
2023-11-09 23:32:57 +0700

    Assesment

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerButton = document.getElementById('hamburger');
+    document.getElementById("product-list").style.display= "none";
+    document.getElementById("button");
+    button.addEventListener("click", () => {
+        document.getElementById("product-list").style.display = "";
+});
+});
+
+
